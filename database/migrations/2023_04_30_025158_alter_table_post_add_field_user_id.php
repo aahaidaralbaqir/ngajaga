1e4cf0dbcae3e34a32a0609b85e716027ab42802
ahaidaralbaqir <ahmad.albaqir@gojek.com>
2023-04-30 09:58:55 +0700

    Add field user id

---
+++ database/migrations/2023_04_30_025158_alter_table_post_add_field_user_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTablePostAddFieldUserId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('post', function(Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->after('banner')->nullable(false);
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post', function(Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
