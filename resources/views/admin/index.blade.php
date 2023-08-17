0ee52fc8b911c7407023c4e39ca8288c7c63d5db
ahmad.albaqir <ahmad.albaqir@gojek.com>
2023-08-17 23:31:07 +0700

    Remove unused code

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -30,13 +30,6 @@
 				  </h4>
 				  <span class="text-sm font-medium">Total Dana Masuk</span>
 				</div>
-
-				<span class="flex items-center gap-1 text-sm font-medium text-meta-3">
-				  0.43%
-				  <svg class="fill-meta-3" width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M4.35716 2.47737L0.908974 5.82987L5.0443e-07 4.94612L5 0.0848689L10 4.94612L9.09103 5.82987L5.64284 2.47737L5.64284 10.0849L4.35716 10.0849L4.35716 2.47737Z" fill=""></path>
-				  </svg>
-				</span>
 			  </div>
 			</div>
 			<!-- Card Item End -->
@@ -58,13 +51,6 @@
 				  </h4>
 				  <span class="text-sm font-medium">Total Dana Keluar</span>
 				</div>
-
-				<span class="flex items-center gap-1 text-sm font-medium text-meta-3">
-				  4.35%
-				  <svg class="fill-meta-3" width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M4.35716 2.47737L0.908974 5.82987L5.0443e-07 4.94612L5 0.0848689L10 4.94612L9.09103 5.82987L5.64284 2.47737L5.64284 10.0849L4.35716 10.0849L4.35716 2.47737Z" fill=""></path>
-				  </svg>
-				</span>
 			  </div>
 			</div>
 			<!-- Card Item End -->
@@ -86,12 +72,6 @@
 				  <span class="text-sm font-medium">Total Jenis Transaksi</span>
 				</div>
 
-				<span class="flex items-center gap-1 text-sm font-medium text-meta-3">
-				  2.59%
-				  <svg class="fill-meta-3" width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M4.35716 2.47737L0.908974 5.82987L5.0443e-07 4.94612L5 0.0848689L10 4.94612L9.09103 5.82987L5.64284 2.47737L5.64284 10.0849L4.35716 10.0849L4.35716 2.47737Z" fill=""></path>
-				  </svg>
-				</span>
 			  </div>
 			</div>
 			<!-- Card Item End -->
@@ -114,12 +94,6 @@
 				  <span class="text-sm font-medium">Total Users</span>
 				</div>
 
-				<span class="flex items-center gap-1 text-sm font-medium text-meta-5">
-				  0.95%
-				  <svg class="fill-meta-5" width="10" height="11" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M5.64284 7.69237L9.09102 4.33987L10 5.22362L5 10.0849L-8.98488e-07 5.22362L0.908973 4.33987L4.35716 7.69237L4.35716 0.0848701L5.64284 0.0848704L5.64284 7.69237Z" fill=""></path>
-				  </svg>
-				</span>
 			  </div>
 			</div>
 			<!-- Card Item End -->
