9eb891f60955db4e51f495549de47c1a149e5ef2
ahaidaralbaqir <ahmad.albaqir@gojek.com>
2024-06-11 22:25:38 +0700

    add supplier table

---
+++ src/database/migrations/2024_06_11_152414_create_supplier_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSupplierTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 5);
+            $table->string('name', 100);
+            $table->string('email', 64);
+            $table->string('phone_number', 16);
+            $table->text('address');
+            $table->timestamps();
+        });
+
+
+        DB::statement('ALTER TABLE suppliers MODIFY created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP');
+        DB::statement('ALTER TABLE suppliers MODIFY updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
