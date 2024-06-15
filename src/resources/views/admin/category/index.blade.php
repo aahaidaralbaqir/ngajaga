ba91871478afa575b10af9a1c952edef34303556
ahaidaralbaqir <ahmad.albaqir@gojek.com>
2024-06-15 12:19:27 +0700

    rbac category

--- src/resources/views/admin/category/index.blade.php
+++ src/resources/views/admin/category/index.blade.php
@@ -16,13 +16,21 @@
                         </div>
                     </div>
                 </button>
-                <a href="{{ route('category.create.form') }}" class="button text-base bg-[#ff91e7] text-black p-3 rounded border border-black">Buat Kategori</a>
+                @if (in_array(\App\Constant\Permission::CREATE_CATEGORY, $user['permission']))
+                    <a href="{{ route('category.create.form') }}" class="button text-base bg-[#ff91e7] text-black p-3 rounded border border-black">Buat Kategori</a>
+                @endif
             </div>
         </div>
         <div class="tab">
-            <a href="{{ route('product.index') }}" aria-selected="true">Produk</a>
-            <a href="{{ route('category.index') }}" aria-selected="true" class="selected">Kategori</a>
-            <a href="{{ route('shelf.index') }}" aria-selected="true">Rak</a>
+            @if (in_array(\App\Constant\Permission::VIEW_PRODUCT, $user['permission']))
+                <a href="{{ route('product.index') }}" aria-selected="true">Produk</a>
+            @endif
+            @if (in_array(\App\Constant\Permission::VIEW_CATEGORY, $user['permission']))
+                <a href="{{ route('category.index') }}" aria-selected="true" class="selected">Kategori</a>
+            @endif
+            @if (in_array(\App\Constant\Permission::VIEW_SHELF, $user['permission']))
+                <a href="{{ route('shelf.index') }}" aria-selected="true">Rak</a>
+            @endif
         </div>
     </div>
 </div>
@@ -49,6 +57,7 @@
                             <span class="font-bold underline">{{ $item->total_product }}</span>
                         </td>
                         <td class="relative">
+                            @if (in_array(\App\Constant\Permission::UPDATE_CATEGORY, $user['permission']))
                             <a href="" data-id="{{ $item->id }}" data-name="action" class="dropdown" role="dropdown">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis"><circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/></svg>
                             </a>
@@ -57,11 +66,8 @@
                                     <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a2.121 2.121 0 1 1 3 3L12 15l-4 1 1-4Z"/></svg>
                                     Ubah
                                 </a>
-                                <a href="{{ route('category.delete', ['categoryId' => $item->id]) }}" class="menu-item text-red ">
-                                    <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>
-                                    Hapus
-                                </a>
                             </div>
+                            @endif
                         </td>
                     </tr>
                 @endforeach
