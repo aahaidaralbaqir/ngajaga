fabf68113fc9d151b4880a44cf6c3abaead598f2
ahaidaralbaqir <ahmad.albaqir@gojek.com>
2024-06-17 00:49:36 +0700

    finish interaction

--- src/resources/views/admin/purchase/form.blade.php
+++ src/resources/views/admin/purchase/form.blade.php
@@ -96,7 +96,6 @@
                         <h1 class="text-2xl mb-2 font-lighter">Stok dan Produk</h1>
                     </header>
                     <fieldset class="w-3/5 flex flex-col">
-                        @{{ order_items }}
                         <div class="box-w">
                             <div 
                                 v-for="(order_item, idx) in order_items" 
@@ -108,23 +107,23 @@
                                     <div class="flex items-center gap-4">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg>
                                         <div class="text-black">
-                                            <h4 class="text-base font-normal">Tidak Ada Nama Produk</h4>
+                                            <h4 class="text-base font-normal capitalize">@{{ getProductName(idx, order_item.product_id) }}</h4>
                                             <ul class="inline p-0 list-none text-base font-normal">
-                                                <li>Kuantitas</li>
-                                                <li>Satuan</li>
+                                                <li>@{{ order_item.qty <= 0 ? 'Kuantitas' : order_item.qty }}</li>
+                                                <li>@{{ getUnitName(order_item.unit, order_item.product_id) }}</li>
                                             </ul>
                                         </div>
                                     </div>
                                     <div class="actions flex gap-2">
-                                        <button class="button text-base text-black p-3 rounded border border-black relative flex gap-2" role="delete">
+                                        <button class="button text-base text-black p-3 rounded border border-black relative flex gap-2" role="delete" @click.prevent="handleRemoveProduct(idx)">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
                                         </button>
-                                        <button class="hidden button text-base text-black p-3 rounded border border-black relative flex gap-2" role="collapse" aria-state="up">
-                                            <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
+                                        <button @click.prevent="handleToggleCollapse(idx)" class="button text-base text-black p-3 rounded border border-black relative flex gap-2" role="collapse" aria-state="up">
+                                            <svg :class="{'rotate-180': idx == opened_item_idx }" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
                                         </button>
                                     </div>
                                 </div>
-                                <div class="flex flex-col gap-4 box-b mt-4">
+                                <div class="flex flex-col gap-4 box-b mt-4" v-if="opened_item_idx == idx">
                                     <fieldset class="w-full">
                                         <label for="product_id" class="text-[#000000] font-normal">Produk</label>
                                         <select name="product_id" id="product_id"  v-model="order_item.product_id">
@@ -133,7 +132,7 @@
                                                 v-for="product in products"
                                                 :value="product.id"
                                             >
-                                                @{{ product.name }}
+                                               @{{ product.name }}
                                             </option>
                                         </select>
                                     </fieldset>
@@ -141,7 +140,13 @@
                                     <fieldset class="w-full">
                                         <label for="unit_id" class="text-[#000000] font-normal">Satuan</label>
                                         <select name="unit_id" id="unit_id" v-model="order_item.unit">
-                                        <option disabled selected>Pilih Satuan</option> 
+                                            <option value="0" selected>Pilih Satuan</option>
+                                            <option
+                                                v-for="unit in order_item.units"
+                                                :value="unit.id"
+                                            >
+                                            @{{ unit.name }}
+                                            </option> 
                                         </select>
                                     </fieldset>
 
@@ -163,8 +168,8 @@
                             </div>
                         </div>
                         
-                        <div class="{{ empty($item) ? 'border border-dashed bg-[#ffffff] p-8 mt-8' : '' }}">
-                            <button id="btn-add-product" class="w-full mx-auto flex gap-2 items-center justify-center button text-base bg-black hover:bg-[#ff91e7] text-white hover:text-black p-3 rounded border border-black">
+                        <div :class="get_add_button_class">
+                            <button @click.prevent="handleAddNewProduct" id="btn-add-product" class="w-full mx-auto flex gap-2 items-center justify-center button text-base bg-black hover:bg-[#ff91e7] text-white hover:text-black p-3 rounded border border-black">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
                                 Tambah Produk
                             </button>
@@ -181,14 +186,8 @@
     new Vue({
         el: '#purchase',
         data: {
+            opened_item_idx: -1,
             order_items: [
-                {
-                    product_id: 0,
-                    qty: 0,
-                    unit: 0,
-                    price: 0,
-                    note: ''
-                }
             ],
             products: [],
             suppliers: [],
@@ -212,6 +211,47 @@
                     .then((result) => {
                         this.suppliers = result.suppliers 
                 })
+            },
+            handleAddNewProduct() {
+                this.order_items.push({
+                    product_id: 0,
+                    qty: 0,
+                    unit: 0,
+                    price: 0,
+                    note: '',
+                    units: []
+                })
+                this.opened_item_idx++
+            },
+            handleRemoveProduct(idx) {
+                this.order_items = this.order_items.filter((value, index) => index != idx)
+                this.opened_item_idx--
+            },
+            handleToggleCollapse(idx) {
+                if (idx == this.opened_item_idx) 
+                {
+                    this.opened_item_idx = -1
+                    return;
+                }
+                this.opened_item_idx = idx
+            },
+            getProductName(idx, product_id) {
+                const product_detail = this.products.find((item) => item.id == product_id)
+                if (!product_detail) return 'Tidak ada produk yang dipilih'
+                this.order_items[idx].units = product_detail.units
+                return product_detail.name
+            },
+            getUnitName(unit_id, product_id) {
+                const product_detail = this.products.find((item) => item.id == product_id)
+                if (!product_detail) return 'Satuan'
+                const unit_detail = product_detail.units.find((item) => item.id == unit_id)
+                 if (!unit_detail) return 'Satuan'
+                return unit_detail.name
+            },
+            getQuantity(idx, product_id) {
+                const product_detail = this.products.find((item) => item.id == product_id)
+                if (!product_detail) return 'Kuantitas'
+                return this.order_items[idx].qty
             }
         },
         computed: {
@@ -222,49 +262,18 @@
 
                 const supplier = this.suppliers.find((item) => item.id == this.form.supplier_id)
                 return supplier
+            },
+            get_add_button_class () {
+                if (this.order_items.length > 0) {
+                    return 'mt-8'
+                }
+                return 'border border-dashed bg-[#ffffff] p-8 mt-8'
             }
         },
         mounted () {
             this.fetchProducts()
             this.fetchSuppliers()
         }
-    })
-    window.addEventListener('DOMContentLoaded', function () {
-
-        function getOrderItemTotal() {
-            const s = document.querySelectorAll('[role="listitem"]')
-            return s.length
-        }
-        const btnaddproduct = document.getElementById('btn-add-product')
-        const wrapper = document.querySelector('.box-w')
-        btnaddproduct.addEventListener('click', function(event) {
-            event.preventDefault()
-            const classes = ['border', 'border-dashed', 'bg-[#ffffff]', 'p-8']
-            classes.forEach(function (className) {
-                btnaddproduct.parentNode.classList.remove(className)
-            })
-            const samplepurchaseorderitem = document.querySelector('.sample-product')
-            const newpurchaseorder = samplepurchaseorderitem.cloneNode(true)
-            newpurchaseorder.classList.remove('sample-product')
-            newpurchaseorder.classList.remove('hidden')
-            wrapper.append(newpurchaseorder)
-
-            const deleterowbtn = document.querySelectorAll('[role="delete"]')
-            deleterowbtn.forEach(function (row) {
-                row.addEventListener('click', function (event) {
-                    event.preventDefault();
-                    const parentpurchaseorder = row.parentNode.parentElement.parentElement
-                    parentpurchaseorder.remove()
-                    if (getOrderItemTotal() <= 1) {
-                        const classes = ['border', 'border-dashed', 'bg-[#ffffff]', 'p-8']
-                        classes.forEach(function (className) {
-                            btnaddproduct.parentNode.classList.add(className)
-                        }) 
-                    }
-                })
-            })
-        })
-
     })
 </script>
 @endpush
