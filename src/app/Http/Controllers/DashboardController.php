7138bc964cd7aad3076af94de0a6dca786a01eaa
ahaidaralbaqir <ahmad.albaqir@gojek.com>
2024-07-01 11:42:36 +0700

    finish dashboard

--- src/app/Http/Controllers/DashboardController.php
+++ src/app/Http/Controllers/DashboardController.php
@@ -2,14 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use App\Constant\Constant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use stdClass;
 
 class DashboardController extends Controller
 {
 	public function index(Request $request)
 	{
+		$user_param = $request->all();
+		$query = DB::table('transactions AS tr')
+            ->select(DB::raw('COUNT(*) as total_transaction, SUM(price_total - COALESCE(debt.amount, 0)) as total_amount, COALESCE(sum(total_product_qty), 0) AS total_product_qty'))
+            ->leftJoin('debt', 'debt.transaction_id', '=', 'tr.id');
+		$total_purchase = DB::table('purchase_orders AS po')
+			->where('status', Constant::PURCHASE_ORDER_COMPLETED);
+
+		$most_purchased_product = DB::table('transaction_details as td')
+			->join('transactions as t', 'td.transaction_id', '=', 't.id')
+			->join('products as p', 'td.product_id', '=', 'p.id')
+			->join('category as c', 'p.category_id', '=', 'c.id')
+			->select(
+				'td.product_id',
+				'p.name as product_name',
+				'c.name as category_name',
+				'p.image as product_image',
+				DB::raw('SUM(td.qty) as total_qty')
+			)
+			->groupBy('td.product_id', 'p.name', 'c.name', 'p.image')
+			->orderBy('total_qty', 'DESC')
+			->limit(5);
+		foreach ($user_param as $field => $value) {
+			if (in_array($field, ['start_date']) && $value) {
+                $start = sprintf('%s 00:00:00', $value);
+                $query->where('tr.created_at', '>=', $start);
+                $total_purchase->where('po.purchase_date', '>=', strtotime($start));
+				$most_purchased_product->where('t.created_at', '>=', $start);
+            }
+            if (in_array($field, ['end_date']) && $value) {
+                $end = sprintf('%s 23:59:50', $value);
+                $query->where('tr.created_at', '=<', $end);
+				$total_purchase->where('po.purchase_date', '=<', strtotime($end));
+				$most_purchased_product->where('t.created_at', '<=', $end);
+            }
+		}
+		$transaction_summary = $query->first();
+		if (!$transaction_summary) {
+			$transaction_summary = new stdClass();
+			$transaction_summary->total_transaction = 0;
+			$transaction_summary->total_amount = 0;
+			$transaction_summary->total_product_qty = 0;
+		}
+		$debt_summary = DB::table('debt')
+			->addSelect(DB::raw('SUM(debt.amount) AS total_debt'))
+			->first();
+		$total_debt = 0;
+		if ($debt_summary) {
+			$total_debt = $debt_summary->total_debt;
+		}
+		
 		$user_profile = parent::getUser();
+
+		$lowest_product =  DB::table('products AS pr')
+			->select('pr.id', 'pr.name', DB::raw('SUM(st.qty) AS total_qty'))
+			->leftJoin('stock AS st', 'st.product_id', '=', 'pr.id')
+			->groupBy('pr.id', 'pr.name')
+			->having('total_qty', '<', 5)
+			->get();
+
 		$data['user'] = $user_profile;
-		return view('admin.index', $data);
+		return view('admin.index')
+			->with('user', parent::getUser())
+			->with('summary', $transaction_summary)
+			->with('total_debt', $total_debt)
+			->with('lowest_product', $lowest_product)
+			->with('most_purchased_products', $most_purchased_product->get())
+			->with('total_purchase', count($total_purchase->get()))
+			->with('has_filter', $request->query->count() > 0);
 	}
 }
--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,74 +1,88 @@
 @extends('layout.dashboard')
 @section('content')
-<div class="second-nav py-14 px-14 w-full">
+<div class="pt-5 px-14 w-full" style="border: none;">
     <div class="w-4/5">
         <div class="flex justify-between items-center">
-            <h1>
-                Selamat datang di Ngajaga
+            <h1 class="mt-4 text-4xl text-black">
+                Selamat datang
             </h1>
+			<div class="flex items-center justify-between gap-5 relative">
+                   
+                </div>
         </div>
     </div>
 </div>
-<main class="w-full py-14 px-14">
+<main class="w-full px-14 mt-4 pb-10">
     <section class="w-4/5">
+		<div class="greeting">
+			<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path fill-rule="evenodd" clip-rule="evenodd" d="M12 21.61a9.6 9.6 0 1 0 0-19.2 9.6 9.6 0 0 0 0 19.2Zm4.45-11.152a1.2 1.2 0 0 0-1.698-1.697l-3.951 3.952-1.552-1.552a1.2 1.2 0 0 0-1.697 1.697l2.4 2.4a1.2 1.2 0 0 0 1.697 0l4.8-4.8Z" fill="#23A094"></path>
+			</svg>
+			<div class="ml-3 text-sm"> Halo {{ $user['name'] }}</div>
+		</div>
 		@if (in_array(\App\Constant\Permission::VIEW_REPORT_SALE_SUMMARY, $user['permission']))
-			<h2 class="text-black font-normal text-2xl">Laporan Penjualan</h2>
-			<div class="box border-black mt-4">
-				<div class="flex gap-2 items-center justify-between">
-					<div class="flex gap-2">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
-						<h5 class="text-black font-normal">Juni 2024</h5>
-						<h4 class="text-black font-light">(8%) dibanding bulan lalu</h4>
-					</div>
-					<button class="button text-base text-black p-3 rounded border border-black relative">
-						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sliders-horizontal"><line x1="21" x2="14" y1="4" y2="4"/><line x1="10" x2="3" y1="4" y2="4"/><line x1="21" x2="12" y1="12" y2="12"/><line x1="8" x2="3" y1="12" y2="12"/><line x1="21" x2="16" y1="20" y2="20"/><line x1="12" x2="3" y1="20" y2="20"/><line x1="14" x2="14" y1="2" y2="6"/><line x1="8" x2="8" y1="10" y2="14"/><line x1="16" x2="16" y1="18" y2="22"/></svg>
-						<div class="menu hidden top-14 p-10">
-							<div class="flex gap-5">
-								<fieldset>
-									<label for="start" class="text-black">Dari</label>
-									<input type="date" name="" id="">
-								</fieldset>
-								<fieldset>
-									<legend>
-										<label for="start" class="text-black">Dari</label>
-									</legend>
-									<input type="date" name="" id="">
-								</fieldset>
-							</div>
-						</div>
+			<div class="flex justify-between items-center relative">
+				<h2 class="text-black font-normal text-2xl">Laporan Penjualan</h2>
+				<div class="flex gap-5">
+					@if ($has_filter)
+                        <a href="{{ route('dashboard') }}" class="button text-base text-black p-3 rounded border border-black relative">
+                            Hapus Filter
+                        </a>
+                    @endif
+					<button class="button text-base text-black p-3 rounded border border-black relative" role="dropdown" data-id="89" data-name="action">
+                        <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-list-filter"><path d="M3 6h18"/><path d="M7 12h10"/><path d="M10 18h4"/></svg>
 					</button>
+                    <div class="menu top-14 right-1 mt-2 hidden" id="cmenu" data-id="89" data-name="action" role="dropdown-content">
+                        <form action="{{ route('dashboard') }}" method="GET">
+                            <div class="flex gap-5 p-4 border-b border-black">
+                                <fieldset>
+                                    <label for="start" class="text-black">Dari Tanggal</label>
+                                    <div class="flex gap-4 mt-2">
+                                        <input type="date" name="start_date" id="" value="{{ request('start_date') }}">
+                                        <input type="date" name="end_date" id="" value="{{ request('end_date') }}">
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="mt-4 flex px-4">
+                                <button type="submit" class="w-full button text-base bg-[#ff91e7] text-black p-3 rounded border border-black">Saring</a>
+                            </div>
+                        </form>
+                    </div>
 				</div>
+				
+			</div>
+			<div class="box border-black mt-4">
 				<table class="w-full mt-10 text-black">
 					<tbody>
 						<tr>
 							<td rowspan="2" class="border-r p-4">
-								<h4 class=" text-1xl">Total Penjualan</h4>
-								<h3 class="text-3xl">Rp 0</h3>
+								<h4 class=" text-1xl">Total Keuntungan</h4>
+								<h3 class="text-3xl">{{ \App\Util\Common::formatAmount(\App\Constant\Constant::UNIT_NAME_RUPIAH, $summary->total_amount) }}</h3>
 							</td>
 							<td class="border-r p-4">
 								<div class="border-b border-black py-7">
-									<h4 class="text-1xl">Total Keuntungan</h4>
-									<h3 class="text-3xl">Rp 0</h3>
+									<h4 class="text-1xl">Total Transaksi</h4>
+									<h3 class="text-3xl">{{ $summary->total_transaction }}</h3>
 								</div>
 							</td>
 							<td class="p-4">
 								<div class="border-b border-black py-7">
-									<h4 class="text-1xl">Total Keuntungan</h4>
-									<h3 class="text-3xl">Rp 0</h3>
+									<h4 class="text-1xl">Total Produk Terjual</h4>
+									<h3 class="text-3xl">{{ $summary->total_product_qty }}</h3>
 								</div>
 							</td>
 						</tr>
 						<tr>
 							<td class="border-r p-4">
 								<div class="py-7">
-									<h4 class="text-1xl">Total Keuntungan</h4>
-									<h3 class="text-3xl">Rp 0</h3>
+									<h4 class="text-1xl">Total Pembelian</h4>
+									<h3 class="text-3xl">{{ $total_purchase }}</h3>
 								</div>
 							</td>
 							<td class="p-4">
 								<div class="py-7">
-									<h4 class="text-1xl">Total Keuntungan</h4>
-									<h3 class="text-3xl">Rp 0</h3>
+									<h4 class="text-1xl">Total Kasbon</h4>
+									<h3 class="text-3xl">{{ \App\Util\Common::formatAmount(\App\Constant\Constant::UNIT_NAME_RUPIAH, $total_debt) }}</h3>
 								</div>
 							</td>
 						</tr>
@@ -76,6 +90,216 @@
 				</table>
 			</div>
 		@endif
+
+		<div class="flex gap-7 mt-10">
+			<div class="flex-1">
+				<table class="w-full retro">
+					<caption class="text-2xl text-black [text-align:unset]">Produk Terfavorit</caption>
+					<thead>
+						<tr>
+							<th class="w-[15%]"></th>
+							<th class="text-left">Nama Produk</th>
+							<th class="text-left">Jumlah Penjualan</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach ($most_purchased_products as $product)
+							<tr>
+								<td class="cell-image">
+									<img src="{{ \App\Util\Common::getStorage(\App\Constant\Constant::STORAGE_PRODUCT, $product->product_image) }}" alt="">
+								</td>
+								<td>{{ $product->product_name }}</td>
+								<td>{{ $product->total_qty }}</td>
+							</tr>
+						@endforeach
+						@if (count($most_purchased_products) < 1)
+							<tr>
+								<td colspan="3">Tidak ada data</td>
+							</tr>
+						@endif
+					</tbody>
+				</table>
+			</div>
+			<div class="flex-1">
+				<table class="w-full retro">
+					<caption class="text-2xl text-black [text-align:unset]">Stok Menipis</caption>
+					<thead>
+						<tr>
+							<th class="w-[15%]"></th>
+							<th class="text-left">Nama Produk</th>
+							<th class="text-left">Jumlah Stok  Saat Ini</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach ($lowest_product as $lp)
+							<tr>
+								<td class="cell-image">
+									<img src="{{ \App\Util\Common::getStorage(\App\Constant\Constant::STORAGE_PRODUCT, $product->product_image) }}" alt="">
+								</td>
+								<td>{{ $lp->name }}</td>
+								<td>{{ $lp->total_qty }}</td>
+							</tr>
+						@endforeach
+						@if (count($lowest_product) < 1)
+							<tr>
+								<td colspan="3">Tidak ada data</td>
+							</tr>
+						@endif
+					</tbody>
+				</table>	
+			</div>
     </section>
 </main>
 @endsection
+@push('scripts')
+	<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+	<script type="text/javascript">
+		
+		const getChartOptions = () => {
+		return {
+			series: [35.1, 23.5, 2.4, 5.4],
+			colors: ["#1C64F2", "#16BDCA", "#FDBA8C", "#E74694"],
+			chart: {
+			height: 320,
+			width: "100%",
+			type: "donut",
+			},
+			stroke: {
+			colors: ["transparent"],
+			lineCap: "",
+			},
+			plotOptions: {
+			pie: {
+				donut: {
+				labels: {
+					show: true,
+					name: {
+					show: true,
+					fontFamily: "Inter, sans-serif",
+					offsetY: 20,
+					},
+					total: {
+					showAlways: true,
+					show: true,
+					label: "Unique visitors",
+					fontFamily: "Inter, sans-serif",
+					formatter: function (w) {
+						const sum = w.globals.seriesTotals.reduce((a, b) => {
+						return a + b
+						}, 0)
+						return '$' + sum + 'k'
+					},
+					},
+					value: {
+					show: true,
+					fontFamily: "Inter, sans-serif",
+					offsetY: -20,
+					formatter: function (value) {
+						return value + "k"
+					},
+					},
+				},
+				size: "80%",
+				},
+			},
+			},
+			grid: {
+			padding: {
+				top: -2,
+			},
+			},
+			labels: ["Direct", "Sponsor", "Affiliate", "Email marketing"],
+			dataLabels: {
+			enabled: false,
+			},
+			legend: {
+			position: "bottom",
+			fontFamily: "Inter, sans-serif",
+			},
+			yaxis: {
+			labels: {
+				formatter: function (value) {
+				return value + "k"
+				},
+			},
+			},
+			xaxis: {
+			labels: {
+				formatter: function (value) {
+				return value  + "k"
+				},
+			},
+			axisTicks: {
+				show: false,
+			},
+			axisBorder: {
+				show: false,
+			},
+			},
+		}
+		}
+
+		if (document.getElementById("donut-chart") && typeof ApexCharts !== 'undefined') {
+		const chart = new ApexCharts(document.getElementById("donut-chart"), getChartOptions());
+		chart.render();
+
+		// Get all the checkboxes by their class name
+		const checkboxes = document.querySelectorAll('#devices input[type="checkbox"]');
+
+		// Function to handle the checkbox change event
+		function handleCheckboxChange(event, chart) {
+			const checkbox = event.target;
+			if (checkbox.checked) {
+				switch(checkbox.value) {
+					case 'desktop':
+					chart.updateSeries([15.1, 22.5, 4.4, 8.4]);
+					break;
+					case 'tablet':
+					chart.updateSeries([25.1, 26.5, 1.4, 3.4]);
+					break;
+					case 'mobile':
+					chart.updateSeries([45.1, 27.5, 8.4, 2.4]);
+					break;
+					default:
+					chart.updateSeries([55.1, 28.5, 1.4, 5.4]);
+				}
+
+			} else {
+				chart.updateSeries([35.1, 23.5, 2.4, 5.4]);
+			}
+		}
+
+		// Attach the event listener to each checkbox
+		checkboxes.forEach((checkbox) => {
+			checkbox.addEventListener('change', (event) => handleCheckboxChange(event, chart));
+		});
+		}
+
+	</script>
+@endpush
+@push('styles')
+    <style type="text/css">
+        #cmenu::after {	
+            content: "";
+            border-left: solid .5rem rgba(0, 0, 0, 0);
+            border-right: solid .5rem rgba(0, 0, 0, 0);
+            border-bottom: solid .5rem black;
+            position: absolute;
+            top: -9px;
+            left: 95%;
+            transform: translate(-50%, 0);
+            z-index: 30;
+        }
+		.greeting {
+			display: flex;
+			gap: 5;
+			align-items: center;
+			background-color: rgb(35 160 148 / 0.2);
+			color: black;
+			padding: 1rem;
+			border: 1px solid  rgb(35 160 148);
+			border-radius: 4px;
+			margin-bottom: 15px;
+		}
+    </style>
+@endpush
